0fe6759dc8174bd93811d298be93302fff129e70
Petrisor Rares <petrisorrares123@gmail.com>
2026-08-09 00:05:02 +0300

    Limit clause

---
+++ postgresql-tutorial/Filtering Data/limit.sql
@@ -0,0 +1,44 @@
+-- Structure:
+
+-- SELECT
+--   select_list
+-- FROM
+--   table_name
+-- ORDER BY
+--   sort_expression
+-- LIMIT
+--   row_count
+-- OFFSET
+--   row_to_skip;
+
+SELECT
+    film_id,
+    title,
+    release_year
+FROM
+    film
+ORDER BY
+    film_id
+LIMIT
+    5;
+
+ SELECT
+  film_id,
+  title,
+  release_year
+FROM
+  film
+ORDER BY
+  film_id
+LIMIT 4 OFFSET 3;
+
+SELECT
+  film_id,
+  title,
+  rental_rate
+FROM
+  film
+ORDER BY
+  rental_rate DESC
+LIMIT
+    10;
