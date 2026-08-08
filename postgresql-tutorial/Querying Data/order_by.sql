fdae2fb2b3616f80564817e3d96ad59ecd134402
Petrisor Rares <petrisorrares123@gmail.com>
2026-08-08 18:55:49 +0300

    Postgres querying tutorial

---
+++ postgresql-tutorial/Querying Data/order_by.sql
@@ -0,0 +1,32 @@
+SELECT
+    first_name,
+    last_name
+FROM
+    customer
+ORDER BY
+    first_name ASC,
+    last_name DESC;
+
+SELECT
+    first_name,
+    LENGTH(first_name) len 
+FROM
+    customer
+ORDER BY
+    len DESC;
+
+CREATE TABLE sort_demo(num INT);
+
+INSERT INTO sort_demo(num)
+VALUES
+    (1),
+    (2),
+    (3),
+    (null);
+
+SELECT
+  num
+FROM
+  sort_demo
+ORDER BY
+  num DESC NULLS FIRST;
